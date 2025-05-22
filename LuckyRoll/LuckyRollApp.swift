ce1deb30b2addd91ceb07834fe8c000ff19e2539
Adrian Borlido <borlidoadrian@gmail.com>
2025-05-22 00:10:29 -0300

    Initial commit (reset history)

---
+++ LuckyRoll/LuckyRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyRollApp.swift
+//  LuckyRoll
+//
+//  Created by Adrian Borlido on 21/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LuckyRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuckyRollTests/LuckyRollTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyRollTests.swift
+//  LuckyRollTests
+//
+//  Created by Adrian Borlido on 21/5/25.
+//
+
+import Testing
+@testable import LuckyRoll
+
+struct LuckyRollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
